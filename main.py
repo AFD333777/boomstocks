70628e448e114fcdbeeb235c7b8a32c895d00ebc
AFD333777 <62431888+AFD333777@users.noreply.github.com>
2021-04-20 16:11:49 +0300

    BOT + Site

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask"
+    return "Главная страница"
 
 
 if __name__ == '__main__':
---
+++ mainBot.py
@@ -0,0 +1,19 @@
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler
+
+
+def echo(update, context):
+    update.message.reply_text("Я получил сообщение: " + update.message.text)
+
+
+def main():
+    updater = Updater("1619648579:AAFZ15uTggnT94_aupP9h0byM5ErkoyRVrs", use_context=True)
+    dp = updater.dispatcher
+    text_handler = MessageHandler(Filters.text, echo)
+    dp.add_handler(text_handler)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
